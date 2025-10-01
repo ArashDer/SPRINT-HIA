6b19fb63e76d5cd4c2e93dc6a8136f3a4749670e
ArashDer <arash_der@protonmail.com>
2025-10-01 11:06:35 +0200

    MetS_HIA_SPRINT.R

--- MetS_HIA_SPRINT.R
+++ MetS_HIA_SPRINT.R
@@ -1,4 +1,6 @@
-
+# R Script example 1 of the SPRINT WP5 HIA
+#Calculation of population attributable fraction of metabolic syndrome
+#the RR comes form the meta-analysis
 
 # Inputs
 baseline_cases <- 4234000   # baseline metabolic syndrome cases
@@ -16,3 +18,4 @@ attr_cases <- AF * baseline_cases
 
 cat("Attributable fraction (AF):", round(AF, 4), "\n")
 cat("Attributable cases of MetS:", round(attr_cases), "\n")
+
